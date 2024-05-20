3db12cbfea806ccb5bea9c8bd55cbadcbe3a6d89
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-20 22:20:46 +0500

    Delete src directory and uploading new

--- src/main/java/com/efrcs/echofriend/models/banner/response/BannerResponse.java
+++
@@ -1,7 +0,0 @@
-package com.efrcs.echofriend.models.banner.response;
-
-import com.efrcs.echofriend.models.baseresponse.EcoFriendlyBaseResponse;
-
-public class BannerResponse extends EcoFriendlyBaseResponse {
-    
-}
