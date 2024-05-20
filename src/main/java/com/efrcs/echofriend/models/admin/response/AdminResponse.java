b7339fd990f457a325512af8f0cfe19d3aa5e0ff
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-20 23:11:40 +0500

    Delete src directory
    will be adding new latest src file shortly

--- src/main/java/com/efrcs/echofriend/models/admin/response/AdminResponse.java
+++
@@ -1,9 +0,0 @@
-package com.efrcs.echofriend.models.admin.response;
-
-import com.efrcs.echofriend.models.baseresponse.EcoFriendlyBaseResponse;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class AdminResponse extends EcoFriendlyBaseResponse{
-    
-}
